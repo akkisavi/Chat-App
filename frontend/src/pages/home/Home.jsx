71fa968ece49beb21c5cbbc4ec5d1bdf3f0cdbda
akkisavi <akshaygosavi621@gmail.com>
2024-11-06 16:20:28 +0530

    display height change for better view

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,9 @@ import MessageContainer from "../../components/messages/MessageContainer"
 
 const Home = () => {
   return (<>
-    <div className="flex sm:h-[450px] md:h[-550px] rounded-lg overflow-hidden bg-blue-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60">
+    <div className="flex sm:h-[550px] md:h[-650px] 
+    rounded-lg overflow-hidden bg-blue-400 
+    bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60">
       <SideBar/>
       <MessageContainer/>
     </div>
