b3482f049e5b25b0fd0f3dff03ca075d02481b1c
akkisavi <akshaygosavi621@gmail.com>
2024-11-16 00:20:44 +0530

    vercel deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:8000",
-			},
-		},
-	},
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+      },
+    },
+  },
 });
